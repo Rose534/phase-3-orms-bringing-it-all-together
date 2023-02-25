dba1245e2162a105899fb5eef6a485d0dfe178a8
rose534 <wambuirose534@gmail.com>
2023-02-25 03:39:37 +0300

    lab

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,85 @@
-class Dog
+require 'sqlite3'
 
+class Dog
+  attr_accessor :name, :breed, :id
+
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+      self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
+  end
+
+  def self.create(name:, breed:)
+    dog = self.new(name: name, breed: breed)
+    dog.save
+  end
+
+  def self.new_from_db(row)
+    self.new(name: row[1], breed: row[2], id: row[0])
+  end
+
+  def self.all
+    sql = "SELECT * FROM dogs"
+    rows = DB[:conn].execute(sql)
+    rows.map { |row| self.new_from_db(row) }
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
+    row = DB[:conn].execute(sql, name).first
+    self.new_from_db(row)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM dogs WHERE id = ? LIMIT 1"
+    row = DB[:conn].execute(sql, id).first
+    self.new_from_db(row)
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    sql = "SELECT * FROM dogs WHERE name = ? AND breed = ?"
+    row = DB[:conn].execute(sql, name, breed).first
+    if row
+      self.new_from_db(row)
+    else
+      self.create(name: name, breed: breed)
+    end
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
 end
